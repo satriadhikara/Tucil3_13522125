16d27e07ff4ce236e982eb9ab8cf430f6f9bdfa1
satriadhikara <satriadhikara@gmail.com>
2024-05-06 12:08:28 +0700

    refactor: change the directory using oracle's

--- src/backend/src/main/java/satriadhikara/stima/tucil3/BackendApplication.java
+++ src/backend/src/main/java/satriadhikara/stima/tucil3/BackendApplication.java
@@ -2,11 +2,13 @@ package satriadhikara.stima.tucil3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import satriadhikara.stima.tucil3.helper.Dictionary;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+        Dictionary dictionary = new Dictionary(); // Load words from file
     }
 }
